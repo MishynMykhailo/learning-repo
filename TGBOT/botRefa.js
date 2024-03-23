8e075d8e04b524ab320eadd327b31a6e5e0727f5
MishynMykhailo <misham18092001@gmail.com>
2024-03-23 17:58:41 +0200

    upd

--- TGBOT/botRefa.js
+++ TGBOT/botRefa.js
@@ -10,27 +10,20 @@ bot.basicMethods.setMyCommands([
 bot.basicMethods.hears("start", (context) => {
   context.reply("что делать с зп?", {
     reply_markup: bot.keyboardService.inlineKeyboard([
-      { text: "Отдать ЗП Мише", callback_data: "right" },
-      { text: "Оставить себе", callback_data: "left" },
+      { text: "Регистрация", callback_data: "registration" },
+      { text: "Настройки", callback_data: "settings" },
     ]),
     parse_mode: "MarkDownV2",
   });
 });
-// bot.basicMethods.hears("menu", (context) => {
-//   context.reply("Открываем меню", {
-//     reply_markup: bot.keyboardService
-//       .replyKeyboardMarkup([{ text: "" }, { text: "Левую" }])
-//       .resize_keyboard(),
-//     parse_mode: "MarkDownV2",
-//   });
-// });
-bot.basicMethods.action("привет", (context) => {
-  context.reply(`Пизда отрежу обе ГЫГЫГЫГЫГЫ ${sum()}`);
+
+
+
+bot.basicMethods.action("registration", (context) => {
+  context.reply(`Регистрация`);
 });
-bot.basicMethods.action("right", (context) => {
-  context.reply(`Молодец, я жду `);
+bot.basicMethods.action("settings", (context) => {
+  context.reply(`Настройки`);
 });
-bot.basicMethods.action("left", (context) => {
-  context.reply("Неправильно, отдадим Мише, жадина ты");
-});
+
 bot.startBot();
